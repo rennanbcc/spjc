44db97e23c050d96c26fe70d2d087689b9bcfbf1
rennanbcc <32692044+rennanbcc@users.noreply.github.com>
2019-07-09 12:08:19 -0300

    Update function proxSalaDisponivel.sql

--- 01-documentacao/rennan/function proxSalaDisponivel.sql
+++ 01-documentacao/rennan/function proxSalaDisponivel.sql
@@ -5,7 +5,7 @@ delimiter //
 DROP FUNCTION IF EXISTS DBSPJC.proxSalaDisponivel //
 
 delimiter :
-create function DBSPJC.proxSalaDisponivel(id INTEGER) 
+create function DBSPJC.proxSalaDisponivel(npu VARCHAR(20)) 
 returns varchar(20)
 deterministic
 begin
@@ -13,10 +13,11 @@ begin
 	set proxSala = '';
     
     select sala INTO proxSala
-	from DBSPJC.Audiencia ps
-	where ps.id = id
-		and (ps.hora= '13:00:00' OR ps.hora = '14:00:00' OR ps.hora = '15:00:00' OR ps.hora= '16:00:00' OR ps.hora = '17:00:00' OR ps.hora = '18:00:00');
+	from DBSPJC.Processo ps JOIN DBSPJC.Audiencia aud ON ps.npu = aud.processo
+	where (aud.hora= '13:00:00' OR aud.hora = '14:00:00' OR aud.hora = '15:00:00' OR aud.hora= '16:00:00' OR aud.hora = '17:00:00' OR aud.hora = '18:00:00');
 	   
 	return proxSala;
 
 end; :
+
+select proxSalaDisponivel('00106948920198170001');
