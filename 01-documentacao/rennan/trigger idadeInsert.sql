a7210d8428c5d87a9bf41d89ccf2573372d1b39b
rennanbcc <32692044+rennanbcc@users.noreply.github.com>
2019-07-09 00:52:06 -0300

    Update trigger idadeInsert.sql

--- 01-documentacao/rennan/trigger idadeInsert.sql
+++ 01-documentacao/rennan/trigger idadeInsert.sql
@@ -1,1 +1,22 @@
+delimiter //
+DROP TRIGGER IF EXISTS DBSPJC.idadeInsert; //
 
+create trigger DBSPJC.idadeInsert
+before insert on DBSPJC.Testemunha
+for each row
+begin
+	update DBSPJC.Testemunha
+    set idade = (
+    CASE 
+        WHEN 
+        MONTH(GETDATE()) > MONTH(dataNascimento) -- *1
+        OR
+        -- *2
+        (
+            MONTH(GETDATE()) = MONTH(dataNascimento) 
+            AND DAY(GETDATE()) >= DAY(dataNascimento) 
+        )
+    THEN DATEDIFF(YEAR, dataNascimento, GETDATE()) 
+    ELSE DATEDIFF(YEAR, dataNascimento, GETDATE()) -1 END
+);
+end; //
