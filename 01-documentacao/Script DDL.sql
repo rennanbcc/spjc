30298c8f0e6433bfc0c443e81e185fc416e656f8
rennanbcc <32692044+rennanbcc@users.noreply.github.com>
2019-07-19 11:13:42 -0300

    Update Script DDL.sql

--- 01-documentacao/Script DDL.sql
+++ 01-documentacao/Script DDL.sql
@@ -17,8 +17,10 @@ CREATE TABLE Juizado (
  ,numeroEndereco                 VARCHAR(10) NOT NULL
  ,dataInicio                     DATE NULL
  ,dataFim                        DATE NULL
+ ,salaAudiencia                  VARCHAR(100) NULL
+
  ,PRIMARY KEY (id)
-) ENGINE=InnoDB;
+) ;
 CREATE INDEX FK_Juizado_Endereco ON Juizado (endereco);
 
 CREATE INDEX FK_Juizado_Servidor ON Juizado (servidorChefe);
@@ -28,23 +30,25 @@ CREATE INDEX FK_Juizado_Magistrado ON Juizado (magistrado);
 CREATE TABLE Pauta (
   id                             INTEGER UNSIGNED AUTO_INCREMENT NOT NULL
  ,estadoPauta                    INTEGER UNSIGNED NOT NULL
+ ,juizado                        INTEGER UNSIGNED NOT NULL 
  ,dataAgendamento                DATETIME NOT NULL
  ,qtdeProcesso                   INTEGER UNSIGNED NULL DEFAULT 0
  ,PRIMARY KEY (id)
-) ENGINE=InnoDB;
+) ;
 CREATE INDEX FK_EstadoPauta ON Pauta (estadoPauta);
+CREATE INDEX FK_JuizadoPauta ON Pauta (juizado);
 
 CREATE TABLE Feito (
   id                             INTEGER UNSIGNED AUTO_INCREMENT NOT NULL
  ,nome                           VARCHAR(100) NOT NULL
  ,PRIMARY KEY (id)
-) ENGINE=InnoDB;
+) ;
 CREATE TABLE Fase (
   id                             INTEGER UNSIGNED AUTO_INCREMENT NOT NULL
  ,nome                           VARCHAR(100) NOT NULL
  ,descricao                      VARCHAR(100) NULL
  ,PRIMARY KEY (id)
-) ENGINE=InnoDB;
+) ;
 CREATE TABLE Testemunha (
   id                             INTEGER UNSIGNED AUTO_INCREMENT NOT NULL
  ,nome                           VARCHAR(100) NOT NULL
@@ -53,8 +57,9 @@ CREATE TABLE Testemunha (
  ,telefone                       VARCHAR(100) NOT NULL
  ,numeroEndereco                 VARCHAR(10) NULL
  ,dataNascimento                 DATE NULL
+ ,idade							 INTEGER
  ,PRIMARY KEY (id)
-) ENGINE=InnoDB;
+) ;
 CREATE TABLE Execucao (
   id                             INTEGER UNSIGNED AUTO_INCREMENT NOT NULL
  ,processo                       VARCHAR(20) NOT NULL
@@ -62,8 +67,10 @@ CREATE TABLE Execucao (
  ,dataRegistro                   DATETIME NOT NULL
  ,valorTotalDivida               DECIMAL(10,2) NOT NULL
  ,dataCumprimento                DATETIME NULL
+ ,numeroParcelasPagas			 INTEGER UNSIGNED NOT NULL DEFAULT 0
+ ,pago                     		 BOOL NOT NULL DEFAULT FALSE
  ,PRIMARY KEY (id)
-) ENGINE=InnoDB;
+) ;
 CREATE INDEX FK_Execucao_Processo ON Execucao (processo);
 
 CREATE TABLE Processo (
@@ -74,7 +81,7 @@ CREATE TABLE Processo (
  ,dataBaixa                      DATE NULL
  ,tipoBaixa                      CHAR(1) NULL
  ,PRIMARY KEY (npu)
-) ENGINE=InnoDB;
+) ;
 CREATE INDEX FK_Processo_Juizado ON Processo (juizado);
 
 CREATE TABLE Magistrado (
@@ -85,7 +92,7 @@ CREATE TABLE Magistrado (
  ,senha                          VARCHAR(512) NOT NULL
  ,numeroEndereco                 VARCHAR(10) NULL
  ,PRIMARY KEY (cpf)
-) ENGINE=InnoDB;
+) ;
 CREATE INDEX FK_Magistrado_Endereco ON Magistrado (endereco);
 
 CREATE TABLE Audiencia (
@@ -100,7 +107,7 @@ CREATE TABLE Audiencia (
  ,situacao                       CHAR(1) NOT NULL
  ,hora                           TIME NOT NULL
  ,PRIMARY KEY (id)
-) ENGINE=InnoDB;
+) ;
 CREATE INDEX FK_Audiencia_Processo ON Audiencia (processo);
 
 CREATE INDEX FK_Audiencia_EstadoAudiencia ON Audiencia (estadoAudiencia);
@@ -118,17 +125,17 @@ CREATE TABLE Endereco (
  ,estado                         CHAR(2) NOT NULL
  ,cidade                         VARCHAR(100) NOT NULL
  ,PRIMARY KEY (cep)
-) ENGINE=InnoDB;
+) ;
 CREATE TABLE EstadoPauta (
   id                             INTEGER UNSIGNED AUTO_INCREMENT NOT NULL
  ,nome                           VARCHAR(100) NOT NULL
  ,PRIMARY KEY (id)
-) ENGINE=InnoDB;
+) ;
 CREATE TABLE ProcessoFeito (
   feito                          INTEGER UNSIGNED NOT NULL
  ,processo                       VARCHAR(20) NOT NULL
  ,PRIMARY KEY (feito, processo)
-) ENGINE=InnoDB;
+) ;
 CREATE INDEX FK_FeitoProcesso_Feito ON ProcessoFeito (feito);
 
 CREATE INDEX FP_FeitoProcesso_Processo ON ProcessoFeito (processo);
@@ -142,24 +149,24 @@ CREATE TABLE Recebimento (
  ,dataPagamento                  DATE NULL
  ,observacao                     VARCHAR(512) NULL
  ,PRIMARY KEY (id)
-) ENGINE=InnoDB;
+) ;
 CREATE INDEX FK_Recebimento_Execucao ON Recebimento (execucao);
 
 CREATE TABLE EstadoAudiencia (
   id                             INTEGER UNSIGNED AUTO_INCREMENT NOT NULL
  ,nome                           VARCHAR(100) NOT NULL
  ,PRIMARY KEY (id)
-) ENGINE=InnoDB;
+) ;
 CREATE TABLE TipoMovimentacao (
   id                             INTEGER UNSIGNED AUTO_INCREMENT NOT NULL
  ,nome                           VARCHAR(100) NOT NULL
  ,PRIMARY KEY (id)
-) ENGINE=InnoDB;
+) ;
 CREATE TABLE TipoDocumento (
   id                             INTEGER UNSIGNED AUTO_INCREMENT NOT NULL
  ,nome                           VARCHAR(100) NOT NULL
  ,PRIMARY KEY (id)
-) ENGINE=InnoDB;
+) ;
 CREATE TABLE Servidor (
   cpf                            VARCHAR(11) NOT NULL
  ,juizado                        INTEGER UNSIGNED NULL
@@ -172,7 +179,7 @@ CREATE TABLE Servidor (
  ,dataInicio                     DATE NULL
  ,dataFim                        DATE NULL
  ,PRIMARY KEY (cpf)
-) ENGINE=InnoDB;
+) ;
 CREATE INDEX FK_Servidor_Endereco ON Servidor (endereco);
 
 CREATE INDEX FK_Servidor_Juizado ON Servidor (juizado);
@@ -185,14 +192,14 @@ CREATE TABLE CursoEspecializacao (
  ,dataConclusao                  DATE NULL
  ,cargaHoraria                   INTEGER UNSIGNED NULL
  ,PRIMARY KEY (id)
-) ENGINE=InnoDB;
+) ;
 CREATE INDEX FK_CursoEspecializacao_Servidor ON CursoEspecializacao (servidor);
 
 CREATE TABLE ProcessoTestemunha (
   processo                       VARCHAR(20) NOT NULL
  ,testemunha                     INTEGER UNSIGNED NOT NULL
  ,PRIMARY KEY (processo, testemunha)
-) ENGINE=InnoDB;
+) ;
 CREATE INDEX FK_ProcessoTestemunha_Pocesso ON ProcessoTestemunha (processo);
 
 CREATE INDEX FK_ProcessoTestemunha_Testemunha ON ProcessoTestemunha (testemunha);
@@ -204,7 +211,7 @@ CREATE TABLE Movimentacao (
  ,tipoMovimentacao               INTEGER UNSIGNED NOT NULL
  ,dataHota                       DATETIME NOT NULL
  ,PRIMARY KEY (id)
-) ENGINE=InnoDB;
+) ;
 CREATE INDEX FK_Movimentacao_TipoMovimentacao ON Movimentacao (tipoMovimentacao);
 
 CREATE INDEX FK_Movimentacao_Servidor ON Movimentacao (servidor);
@@ -218,7 +225,7 @@ CREATE TABLE ProcessoFase (
  ,dataRegistro                   DATETIME NOT NULL
  ,faseAtual                      BOOL NOT NULL DEFAULT false
  ,PRIMARY KEY (id)
-) ENGINE=InnoDB;
+) ;
 CREATE INDEX FK_ProcessoFase_Processo ON ProcessoFase (processo);
 CREATE INDEX FK_ProcessoFase_Fase ON ProcessoFase (fase);
 CREATE UNIQUE INDEX UN_Fase_Processo ON ProcessoFase (fase, processo);
@@ -237,7 +244,7 @@ CREATE TABLE Documento (
  ,assinatura                     VARCHAR(512) NULL
  ,dataAssinatura                 DATETIME NULL
  ,PRIMARY KEY (id)
-) ENGINE=InnoDB;
+) ;
 CREATE INDEX FK_Documento_TipoDocumento ON Documento (tipoDocumento);
 
 CREATE INDEX FH_Documento_Documento ON Documento (documentoPrincipal);
@@ -264,7 +271,7 @@ CREATE TABLE Representante (
  ,tipoRepresentante              CHAR(1) NULL
  ,matricula                      INTEGER UNSIGNED NULL
  ,PRIMARY KEY (cpf)
-) ENGINE=InnoDB;
+) ;
 CREATE INDEX FK_Representante_Endereco ON Representante (endereco);
 
 
@@ -272,7 +279,7 @@ CREATE TABLE ProcessoRepresentante (
   processo                       VARCHAR(20) NOT NULL
  ,representante                  VARCHAR(11) NOT NULL
  ,PRIMARY KEY (processo, representante)
-) ENGINE=InnoDB;
+) ;
 CREATE INDEX FK_ProcessoRepresentante_Processo ON ProcessoRepresentante (processo);
 
 CREATE INDEX FK_ProcessoRepresentante_Representante ON ProcessoRepresentante (representante);
@@ -286,7 +293,7 @@ CREATE TABLE CorrespondenciaParte (
  ,numeroAR                       VARCHAR(10) NULL
  ,dataHoraRecebimento            DATETIME NULL
  ,PRIMARY KEY (id)
-) ENGINE=InnoDB;
+) ;
 CREATE INDEX FK_CorrespondenciaParte_ComunicacaoParte ON CorrespondenciaParte (comunicacaoParte);
 
 CREATE TABLE Parte (
@@ -296,12 +303,12 @@ CREATE TABLE Parte (
  ,telefone                       VARCHAR(20) NULL
  ,nemeroEndereco                 VARCHAR(10) NULL
  ,PRIMARY KEY (id)
-) ENGINE=InnoDB;
+) ;
 CREATE TABLE ProcessoParte (
   processo                       VARCHAR(20) NOT NULL
  ,parte                          INTEGER UNSIGNED NOT NULL
  ,PRIMARY KEY (processo, parte)
-) ENGINE=InnoDB;
+) ;
 CREATE INDEX FK_ProcessoParte_processo ON ProcessoParte (processo);
 
 CREATE INDEX FK_ProcessoParte_Parte ON ProcessoParte (parte);
@@ -315,7 +322,7 @@ CREATE TABLE CorrespondenciaTestemunha (
  ,numeroAR                       VARCHAR(10) NULL
  ,dataHoraRecebimento            DATETIME NULL
  ,PRIMARY KEY (id)
-) ENGINE=InnoDB;
+) ;
 CREATE INDEX FK_CorrespondenciaTestemunha_ComunicacaoTestemumha ON CorrespondenciaTestemunha (comunicacaoTestemunha);
 
 CREATE TABLE ComunicacaoParte (
@@ -323,7 +330,7 @@ CREATE TABLE ComunicacaoParte (
  ,parte                          INTEGER UNSIGNED NOT NULL
  ,servidor                       VARCHAR(11) NOT NULL
  ,PRIMARY KEY (id)
-) ENGINE=InnoDB;
+) ;
 CREATE INDEX FK_ComunicacaoParte_Servidor ON ComunicacaoParte (servidor);
 
 CREATE INDEX FK_ComunicacaoParte_Parte ON ComunicacaoParte (parte);
@@ -333,7 +340,7 @@ CREATE TABLE ComunicacaoTestemunha (
  ,servidor                       VARCHAR(11) NOT NULL
  ,testemunha                     INTEGER UNSIGNED NOT NULL
  ,PRIMARY KEY (id)
-) ENGINE=InnoDB;
+) ;
 CREATE INDEX FK_ComunicacaoTestemunha_Servidor ON ComunicacaoTestemunha (servidor);
 
 CREATE INDEX FK_ComunicacaoTestemunha_Testemunha ON ComunicacaoTestemunha (testemunha);
@@ -348,7 +355,7 @@ CREATE TABLE ParteFisica (
  ,tipoDocumentoIdentificacao     VARCHAR(10) NULL
  ,documentoIdentificacao         VARCHAR(50) NULL
  ,PRIMARY KEY (parte)
-) ENGINE=InnoDB;
+) ;
 CREATE INDEX FK_ParteFisica_Parte ON ParteFisica (parte);
 
 CREATE TABLE ParteJuridica (
@@ -356,8 +363,8 @@ CREATE TABLE ParteJuridica (
  ,cnpj                           VARCHAR(20) NULL
  ,razaoSocial                    VARCHAR(100) NULL
  ,PRIMARY KEY (parte)
-) ENGINE=InnoDB;
-CREATE INDEX FK_ParteFisica_Parte ON ParteJuridica (parte);
+) ;
+CREATE INDEX FK_ParteJuridica_Parte ON ParteJuridica (parte);
 
 ALTER TABLE Juizado
   ADD CONSTRAINT Rel_Endereco_Juizado FOREIGN KEY (endereco)
